17cdd9ad3f2bdc2508b95dc1900f952fac82f001
Rafael Garcia <rafa@myariadna.com>
2017-09-29 13:33:10 +0200

    office copy [1]

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, AlertController, Platform, ModalController } from 'ionic-angular';
+import { PhotoModel } from '../../models/photo-model';
+import { SimpleAlertProvider } from '../../providers/simple-alert/simple-alert';
+import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file';
+import { DataProvider } from '../../providers/data/data';
+
+declare var cordova;
 
 @IonicPage()
 @Component({
@@ -8,8 +15,54 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  loaded: boolean = false;
+  photoTaken: boolean = false;
+  photos: PhotoModel[] = [];
 
+  constructor(public dataService: DataProvider, public platform: Platform,
+    public simpleAlert: SimpleAlertProvider, public modalCtrl:
+      ModalController, public alertCtrl: AlertController, public camera:
+      Camera, public file: File) {
+
+  }
+
+  ionViewDidLoad() {
+    // Uncomment to use test data
+    /*this.photos = [
+    new PhotoModel('http://placehold.it/100x100', new Date()),
+    new PhotoModel('http://placehold.it/100x100', new Date()),
+    new PhotoModel('http://placehold.it/100x100', new Date())
+    ]*/
+
+    this.platform.ready().then(() => {
+      this.loadPhotos();
+    });
+
+    this.platform.resume.subscribe(() => {
+      if (this.photos.length > 0) {
+        let today = new Date();
+        if (this.photos[0].date.setHours(0,0,0,0) == today.setHours(0,0,0,0)){
+          this.photoTaken = true;
+        }else{
+          this.photoTaken = false;
+        }
+      }
+    })
+  }
+
+  loadPhotos(): void {
+  }
+  takePhoto(): any {
+  }
+  createPhoto(photo): void {
+  }
+  removePhoto(photo): void {
+  }
+  playSlideshow(): void {
+  }
+  sharePhoto(image): void {
+  }
+  save(): void {
   }
 
 }
--- src/providers/simple-alert/simple-alert.ts
+++ src/providers/simple-alert/simple-alert.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class SimpleAlertProvider {
 
-  constructor() {
+  constructor(public alertCtrl: AlertController) {
+  }
+
+  create(title:string, message: string): any {
+    return this.alertCtrl.create({
+      title: title,
+      message: message,
+      buttons:[
+        {
+          text: 'Ok'
+        }
+      ]
+    })
   }
 
 }
